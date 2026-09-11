587dcc563e0ed361ca6df1424efa489c62c1415d
gvvettorazzi <gabriel.vettorazzi@pucpr.edu.br>
2026-09-11 01:32:10 -0300

    Update ClientFixtures.php

--- src/DataFixtures/ClientFixtures.php
+++ src/DataFixtures/ClientFixtures.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
 use App\Entity\Client;
@@ -13,19 +15,22 @@ use App\Entity\ProjectType;
 use App\Entity\Task;
 use App\Entity\TaskStatus;
 use App\Entity\User;
-use DateTime;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-use Faker;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class ClientFixtures extends Fixture implements DependentFixtureInterface
+final class ClientFixtures extends Fixture implements DependentFixtureInterface
 {
-    protected Faker\Generator $faker;
+    private Generator $faker;
 
-    public function __construct()
-    {
-        $this->faker = Faker\Factory::create();
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+        $this->faker = Factory::create();
     }
 
     public function load(ObjectManager $manager): void
@@ -36,68 +41,118 @@ class ClientFixtures extends Fixture implements DependentFixtureInterface
         $taskStatuses = $manager->getRepository(TaskStatus::class)->findAll();
         $files = $manager->getRepository(File::class)->findAll();
 
-        for ($j = 0; $j < 10; $j++) {
+        for ($clientIndex = 0; $clientIndex < 10; ++$clientIndex) {
             $client = new Client();
-            $client->setUsername($this->faker->email());
-            $client->setPassword(md5($this->faker->email()));
-            $client->setToken($this->faker->ean13());
-            $client->setTokenCreatedAt(new DateTime());
-            $client->setName(sprintf('%s %s', $this->faker->firstName(), $this->faker->lastName()));
+
+            $email = $this->faker->unique()->safeEmail();
+            $plainPassword = bin2hex(random_bytes(24));
+
+            $client->setUsername($email);
+            $client->setPassword(
+                $this->passwordHasher->hashPassword(
+                    $client,
+                    $plainPassword
+                )
+            );
+
+            $client->setToken(bin2hex(random_bytes(32)));
+            $client->setTokenCreatedAt(new DateTimeImmutable());
+            $client->setName(
+                sprintf(
+                    '%s %s',
+                    $this->faker->firstName(),
+                    $this->faker->lastName()
+                )
+            );
             $client->setDescription($this->faker->text());
-            $client->setCreatedAt($this->faker->dateTimeBetween('-30 days', '+0 days'));
+            $client->setCreatedAt(
+                $this->faker->dateTimeBetween('-30 days')
+            );
 
             $manager->persist($client);
 
             $phone = new Contact();
-            $phone->setContactType($manager->getRepository(ContactType::class)->find(ContactType::TYPE_PHONE));
+            $phone->setContactType(
+                $manager
+                    ->getRepository(ContactType::class)
+                    ->find(ContactType::TYPE_PHONE)
+            );
             $phone->setValue($this->faker->phoneNumber());
             $phone->setClient($client);
+
             $manager->persist($phone);
 
-            $email = new Contact();
-            $email->setContactType($manager->getRepository(ContactType::class)->find(ContactType::TYPE_EMAIL));
-            $email->setValue($this->faker->email());
-            $email->setClient($client);
-            $manager->persist($email);
+            $emailContact = new Contact();
+            $emailContact->setContactType(
+                $manager
+                    ->getRepository(ContactType::class)
+                    ->find(ContactType::TYPE_EMAIL)
+            );
+            $emailContact->setValue($this->faker->safeEmail());
+            $emailContact->setClient($client);
 
-            for ($k = 0; $k < 3; $k++) {
+            $manager->persist($emailContact);
+
+            for ($projectIndex = 0; $projectIndex < 3; ++$projectIndex) {
                 $project = new Project();
                 $project->setName($this->faker->name());
                 $project->setClient($client);
-                $project->setType($this->faker->randomElement($projectTypes));
-                $project->setStatus($this->faker->randomElement($projectStatuses));
+                $project->setType(
+                    $this->faker->randomElement($projectTypes)
+                );
+                $project->setStatus(
+                    $this->faker->randomElement($projectStatuses)
+                );
+
                 $manager->persist($project);
 
-                for ($n = 0; $n < 3; $n++) {
+                for ($taskIndex = 0; $taskIndex < 3; ++$taskIndex) {
                     $task = new Task();
-                    $task->setTimeEstimated($this->faker->numberBetween(15, 30));
-                    $task->setTimeSpent($this->faker->numberBetween(15, 30));
+                    $task->setTimeEstimated(
+                        $this->faker->numberBetween(15, 30)
+                    );
+                    $task->setTimeSpent(
+                        $this->faker->numberBetween(15, 30)
+                    );
                     $task->setName($this->faker->name());
                     $task->setProject($project);
-                    $task->setAssignee($this->faker->randomElement($users));
-                    $task->setStatus($this->faker->randomElement($taskStatuses));
-                    $task->setDeadline($this->faker->dateTimeBetween('-30 days', '+30 days'));
+                    $task->setAssignee(
+                        $this->faker->randomElement($users)
+                    );
+                    $task->setStatus(
+                        $this->faker->randomElement($taskStatuses)
+                    );
+                    $task->setDeadline(
+                        $this->faker->dateTimeBetween(
+                            '-30 days',
+                            '+30 days'
+                        )
+                    );
+
                     $manager->persist($task);
                 }
 
-                for ($n = 0; $n < 3; $n++) {
+                for ($documentIndex = 0; $documentIndex < 3; ++$documentIndex) {
                     $document = new Document();
                     $document->setName($this->faker->colorName());
-                    $document->addFile($this->faker->randomElement($files));
+                    $document->addFile(
+                        $this->faker->randomElement($files)
+                    );
                     $document->setClient($client);
+
                     $manager->persist($document);
                 }
             }
-
-            $manager->flush();
         }
+
+        $manager->flush();
     }
 
     public function getDependencies(): array
     {
-        return array(
+        return [
             FileFixtures::class,
             UserFixtures::class,
-        );
+        ];
     }
 }
